7255f7a6714c160f8d091c2fc34b2c377a1f3cf7
abhishekkhanna2710 <abhikhanna2710@gmail.com>
2023-04-29 13:25:42 +0530

    Home page is created with modal function

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,17 @@
 import React from "react"
 import Navbar from "./Components/Navbar/Navbar"
+import Home from "./Pages/Home/Home"
+import { Route, Routes } from "react-router-dom"
 
 function App() {
 
   return (
     <div>
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+      </Routes>
     </div>
   )
 }
